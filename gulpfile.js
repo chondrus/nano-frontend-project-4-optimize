b2985c6ca8437f92d7efa42bfa1618cd1ea774f1
kels <chondrus@users.noreply.github.com>
2015-11-07 18:03:04 -0500

    now using gulp to manage changed; moved files gulp will work off of into 'src' directory, and generated files either in top dir (html) or in 'build' directory

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+// gulp from https://discussions.udacity.com/t/gulp-and-setting-up-a-gulp-workflow-intermediate/24359/3
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css'),
+    minifyhtml = require('gulp-minify-html'),
+    // htmlreplace = require('gulp-html-replace'),
+    // inline = require('gulp-inline-source'),
+    rename = require('gulp-rename');
+
+
+// minifies & concatenates js and outputs it to build/js/app.min.js
+gulp.task('scripts', function(){
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('./build/js'));
+});
+
+// minifies css files and outputs them to build/css/*.css
+gulp.task('styles', function(){
+    return gulp.src('src/css/*.css')
+                .pipe(minifyCSS())
+                .pipe(gulp.dest('./build/css'));
+});
+
+// minifies HTML files and outputs them to *.html
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+        .pipe(minifyhtml({
+            empty: true,
+            quotes: true
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+// do both of those things whenever they change
+// gulp.task('watch', function(){
+// gulp.watch('js/*.js', ['scripts']);
+// gulp.watch('css/**/*.css', ['styles']);
+// });
+
+gulp.task('default', ['scripts', 'styles', 'html']);
